bfe1eb956a5a0c1a2776c47a10016e23b29de62a
mikeli1117 <mikeli1117@gmail.com>
2023-02-03 20:54:46 +0800

    create blank gui

--- crawler2.py
+++ crawler2.py
@@ -2,8 +2,17 @@ import urllib.request as req
 import bs4
 import json
 import os
+import tkinter
 
 url = "https://api.ra3battle.cn/api/server/status/detail"
+fav_map = ["诞生于黄土高原的中华民族（抢地图）.map", "200w苦战无人岛困难版.map", "小岛登陆战.map", 
+"小岛登陆战2.0.map", "苦战无人岛路人版.map", "解放战争正式版.map"]
+fav_mode = ["塔防", ""]
+
+window = tkinter.Tk()
+window.title('RA3 Battlenet')
+window.geometry('550x300')
+window.iconbitmap('favicon.ico')
 
 
 def getJson():
@@ -14,9 +23,7 @@ def getJson():
     data = json.loads(data)
     return data
 
-
-
-def main():
+def print_data():
     data = getJson()
     for i in data["games"]:
         if (i['mod'] == "RA3" and i['gamemode'] == "openstaging"):
@@ -30,15 +37,17 @@ def main():
                 print(j['name'] + " ", end='')
             print()
 
-
             # Map name
             map = os.path.basename(os.path.normpath(i['mapname']))
             print(map)
             print()
-            print()
 
-
+def main():
+    print_data()
     
+    # window["background"] = "#C9C9C9"
+    window.mainloop()
+        
 
 if __name__ == "__main__":
     main()
