e46d11ef49a5ec874fa42c398821961c50b1ca4d
mikeli1117 <mikeli1117@gmail.com>
2023-02-03 17:44:16 +0800

    list room, player, map

--- crawler2.py
+++ crawler2.py
@@ -1,6 +1,7 @@
 import urllib.request as req
 import bs4
 import json
+import os
 
 url = "https://api.ra3battle.cn/api/server/status/detail"
 
@@ -17,8 +18,25 @@ def getJson():
 
 def main():
     data = getJson()
-    for i in data["players"]:
-        print(i['name'])
+    for i in data["games"]:
+        if (i['mod'] == "RA3" and i['gamemode'] == "openstaging"):
+            # Room name
+            x = i['hostname'].split()
+            print("Room: " + x[1] )
+
+            #Player name
+            print("Player: ", end='')
+            for j in i['players']:
+                print(j['name'] + " ", end='')
+            print()
+
+
+            # Map name
+            map = os.path.basename(os.path.normpath(i['mapname']))
+            print(map)
+            print()
+            print()
+
 
     
 
