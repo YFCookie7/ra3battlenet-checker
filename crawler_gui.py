e0a4142a42c126d020c04c31c7ff3a5788e3b4e6
mikeli1117 <mikeli1117@gmail.com>
2023-02-10 13:26:52 +0800

    background blink + sound

--- crawler_gui.py
+++ crawler_gui.py
@@ -5,11 +5,10 @@ from time import sleep
 import tkinter as tk
 import threading
 from playsound import playsound
-# playsound('.mp3')
 
 url = "https://api.ra3battle.cn/api/server/status/detail"
 keyword = ["200w苦战无人岛困难版.map", "小岛登陆战.map", 
-"小岛登陆战2.0.map", "苦战无人岛路人版.map", "解放战争正式版.map", "塔防", "pve", "电脑", "人机"]
+"小岛登陆战2.0.map", "苦战无人岛路人版.map", "解放战争正式版.map", "塔防", "pve", "电脑", "人机", "213"]
 
 # Create Tkinter GUI
 window = tk.Tk()
@@ -29,51 +28,71 @@ def getJson():
     data = json.loads(data)
     return data
 
+def sound():
+    playsound('beep-01.mp3')
+
 # Main loop to update result
 def print_data():
+    color = 0
     while(True):
         os.system('cls')
         data = getJson()
         result = ""
-        window.configure(bg='#F0F0F0')
+        # print("Refreshed")
         
         # Check all game room one by one
         for i in data["games"]:
             # Filiter to RA3 waiting room
             if (i['mod'] == "RA3" and i['gamemode'] == "openstaging"):
+
                 # x[1]=room name, map=map name
                 x = i['hostname'].split()
                 x[1]=x[1].lower()
                 map = os.path.basename(os.path.normpath(i['mapname']))
 
                 # Compare room/map name with keyword
-                found_any=False
+                found=False
                 for index in range(len(keyword)):
                     if (check_word(x[1], keyword[index]) or check_word(map, keyword[index])):
-                        #Set background to red if found 
-                        window.configure(bg='red')
+                        found=True
+                        # print("Found")
                         # playsound('beep-01.mp3')
+                if (found==True):
+                    color+=1
+                    if(color%2==1):
+                        window.configure(bg='red')
+                        thread = threading.Thread(target=sound)
+                        thread.start()
+                        thread.join()
+                    elif(color%2==0):
+                        window.configure(bg='green')
+                        thread = threading.Thread(target=sound)
+                        thread.start()
+                        thread.join()
+                else:
+                    window.configure(bg='#F0F0F0')
+                    
+                
                     
                 # Room name
-                print("Room: " + x[1] )
+                # print("Room: " + x[1] )
                 result = result + "Room: " + x[1] + "\n"
                 
                 #Player name
-                print("Player: ", end='')
+                # print("Player: ", end='')
                 for j in i['players']:
-                    print(j['name'] + " ", end='')
+                    # print(j['name'] + " ", end='')
                     result = result + j['name'] + "\n"
-                print()
+                # print()
 
                 # Map name
-                print(map)
+                # print(map)
                 result = result + map + "\n\n\n"
-                print()
+                # print()
 
         lbl_result["text"] = result
-
         sleep(3)
-         
+        
 
 # 建立一個子執行緒
 t = threading.Thread(target = print_data)
@@ -83,5 +102,4 @@ t.start()
 lbl_result = tk.Label(window, text="")
 lbl_result.grid(row=3, column=0, columnspan=2)
 lbl_result.config(font=("Courier", 11))
-
 window.mainloop()
