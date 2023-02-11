e603e35c9089eadc10a79e438370038a013c9b2e
mikeli1117 <mikeli1117@gmail.com>
2023-02-11 15:57:31 +0800

    toggle sound

--- crawler_gui.py
+++ crawler_gui.py
@@ -8,17 +8,19 @@ from playsound import playsound
 
 url = "https://api.ra3battle.cn/api/server/status/detail"
 keyword = ["200w苦战无人岛困难版.map", "小岛登陆战.map", 
-"小岛登陆战2.0.map", "苦战无人岛路人版.map", "解放战争正式版.map", "塔防", "pve", "电脑", "人机", "213"]
+"小岛登陆战2.0.map", "苦战无人岛路人版.map", "解放战争正式版.map", "塔防", "pve", "电脑", "人机"]
+sound=True
 
 # Create Tkinter GUI
 window = tk.Tk()
 window.title("RA3 BattleNet")
-window.geometry("400x550")
+window.geometry("350x850")
 window.iconbitmap("favicon.ico")
 
 # Check room name contains keyword
 def check_word(string, word):
     return string.find(word) != -1
+
 
 # Obtain Json data
 def getJson():
@@ -28,17 +30,33 @@ def getJson():
     data = json.loads(data)
     return data
 
-def sound():
-    playsound('beep-01.mp3')
+def btn_sound_onClick():
+    global sound
+    if (sound==True):
+        print(str(sound))
+        btn_sound['text'] = 'Sound: OFF'
+        sound=False
+        return
+    elif (sound==False):
+        print(str(sound))
+        btn_sound['text'] = 'Sound: ON'
+        sound=True
+        return
+
+def play_sound():
+    if (sound==True):
+        playsound('beep-01.mp3')
+
+    
 
 # Main loop to update result
 def print_data():
     color = 0
     while(True):
-        os.system('cls')
+        # os.system('cls')
         data = getJson()
         result = ""
-        # print("Refreshed")
+        print("Refreshed")
         
         # Check all game room one by one
         for i in data["games"]:
@@ -59,16 +77,16 @@ def print_data():
                         # playsound('beep-01.mp3')
                 if (found==True):
                     color+=1
+
                     if(color%2==1):
                         window.configure(bg='red')
-                        thread = threading.Thread(target=sound)
-                        thread.start()
-                        thread.join()
                     elif(color%2==0):
                         window.configure(bg='green')
-                        thread = threading.Thread(target=sound)
-                        thread.start()
-                        thread.join()
+                    
+                    thread = threading.Thread(target=play_sound)
+                    thread.start()
+                    thread.join()
+
                 else:
                     window.configure(bg='#F0F0F0')
                     
@@ -94,12 +112,25 @@ def print_data():
         sleep(3)
         
 
+
+btn_sound = tk.Button(window, text='Sound: ON', command=btn_sound_onClick)
+btn_sound.grid(row=0,column=1)
+btn_sound.grid()
+
+
+lbl_result = tk.Label(window, text="")
+lbl_result.grid(row=1, column=1, sticky="NSEW")
+lbl_result.config(font=("Courier", 11))
+
+
+
 # 建立一個子執行緒
 t = threading.Thread(target = print_data)
 # 執行該子執行緒
 t.start()
 
-lbl_result = tk.Label(window, text="")
-lbl_result.grid(row=3, column=0, columnspan=2)
-lbl_result.config(font=("Courier", 11))
+
+
+
+
 window.mainloop()
