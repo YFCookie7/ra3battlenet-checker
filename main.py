f1a11b2c44d921c24137755380a88f118229502b
YFCookie7 <mikeli1117@gmail.com>
2024-06-28 15:21:01 +0800

    sidebar implementation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import customtkinter as ctk
+import json
+import urllib.request as req
 from sidebar import sidebar
 from tabview import tabview
 from friend import friend
@@ -9,6 +11,8 @@ from searchbar import searchbar
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
+        self.server_url = "https://api.ra3battle.cn/api/server/status/detail"
+        self.data = []
 
         # Configure window
         self.title("RA3 BattleNet")
@@ -16,9 +20,7 @@ class App(ctk.CTk):
         self.geometry("960x680")
         self.attributes("-topmost", False)
         ctk.set_appearance_mode("System")  # Mode: "System" (standard), "Dark", "Light"
-        ctk.set_default_color_theme(
-            "blue"
-        )  # Theme: "blue" (standard), "green", "dark-blue"
+        ctk.set_default_color_theme("blue")  # Theme: "blue" (standard), "green", "dark-blue"
 
         # Layout
         self.sidebar_frame = sidebar(self)
@@ -41,6 +43,24 @@ class App(ctk.CTk):
         self.searchbar.grid(
             row=2, column=1, columnspan=2, sticky="nsew", padx=10, pady=10
         )
+
+        self.fetch_data()
+    
+    def fetch_data(self):
+        # os.system("cls")
+        request = req.Request(self.server_url)
+        with req.urlopen(request) as response:
+            self.data = response.read().decode("utf-8")
+        self.data = json.loads(self.data)
+
+        ra3_player_count = 0
+        for i in self.data["games"]:
+            if i["mod"] == "RA3":
+                ra3_player_count += len(i["players"])
+        self.sidebar_frame.lb_player_count.configure(text=f"{len(self.data['players'])}/{ra3_player_count}")
+
+        # process: player count, track player, friend list, rooms
+        self.after(5000, self.fetch_data)
 
 
 if __name__ == "__main__":
--- sidebar.py
+++ sidebar.py
@@ -4,9 +4,46 @@ import customtkinter as ctk
 class sidebar(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent, width=190, corner_radius=0)
+        self.grid_rowconfigure(1, weight=1)
 
-        # label = ctk.CTkLabel(self, text="Frame 1")
-        # label.pack(pady=10, padx=10)
+        self.lb_logo = ctk.CTkLabel(self, text="RA3 BattleNet", font=ctk.CTkFont(size=20, weight="bold"))
+        self.lb_logo.grid(row=0, column=0, padx=20, pady=(40,10), sticky="n")
 
-        # button = ctk.CTkButton(self, text="Frame 1")
-        # button.pack(pady=10, padx=10)
+        self.lb_player_count = ctk.CTkLabel(
+            self,
+            text="##/##",
+            font=ctk.CTkFont(size=18),
+        )
+        self.lb_player_count.grid(row=1, column=0, padx=20, pady=(40, 10), sticky="n")
+
+        self.lb_theme = ctk.CTkLabel(
+            self,
+            text="Theme:",
+        )
+        self.lb_theme.grid(row=2, column=0, padx=20, pady=(10, 0), sticky="s")
+
+        self.menu_theme = ctk.CTkOptionMenu(
+            self,
+            values=["Light", "Dark", "System"],
+            command=lambda theme:ctk.set_appearance_mode(theme),
+        )
+        self.menu_theme.set("Light")
+        self.menu_theme.grid(row=3, column=0, padx=20, pady=(5,0), sticky="s")
+
+        self.lb_scaling = ctk.CTkLabel(
+            self,
+            text="UI Scaling:",
+        )
+        self.lb_scaling.grid(row=4, column=0, padx=20, pady=(20,0), sticky="s")
+
+        self.menu_scaling = ctk.CTkOptionMenu(
+            self,
+            values=["80%", "90%", "100%", "110%", "120%"],
+            command=lambda scaling:ctk.set_widget_scaling((int(scaling.replace("%", "")) / 100)),
+        )
+        self.menu_scaling.set("100%")
+        self.menu_scaling.grid(row=5, column=0, padx=20, pady=(5,20), sticky="s")
+
+
+
+
