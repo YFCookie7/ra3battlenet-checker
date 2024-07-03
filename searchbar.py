182e57a9e8fa7717f84521ddbc944052786f833d
YFCookie7 <mikeli1117@gmail.com>
2024-07-04 01:22:06 +0800

    download menu selection

--- searchbar.py
+++ searchbar.py
@@ -1,3 +1,4 @@
+import threading
 import webbrowser
 import zipfile
 import customtkinter as ctk
@@ -14,8 +15,6 @@ class SearchBar(ctk.CTkFrame):
         self.parent = parent
         self.tracker = Tracker(parent)
         self.download_window = None
-
-        self.map_url = "https://ra3.z31.xyz/?s="
         self.local_map_path = r"C:\Users\mikel\AppData\Roaming\Red Alert 3\Maps"
 
         self.search_entry = ctk.CTkEntry(self, placeholder_text="")
@@ -62,7 +61,7 @@ class SearchBar(ctk.CTkFrame):
             width=120,
             border_width=2,
             text_color=("gray10", "#DCE4EE"),
-            command=lambda: webbrowser.open(self.map_url + self.search_entry.get()),
+            # command=lambda: webbrowser.open(self.map_url + self.search_entry.get()),
         )
         self.btn_add.grid(row=0, column=4, padx=(5, 5), sticky="nsew")
 
@@ -85,10 +84,10 @@ class SearchBar(ctk.CTkFrame):
 
         pending_dict = {}
 
+        # Fetch available map links
         with open("./map_downloader/map_dict.json", "r", encoding="utf-8") as f:
             map_dict = json.load(f)
 
-        # Fetch available map links
         for map_name, map_url in map_dict.items():
             if target_map in map_name:
                 pending_dict[map_name] = map_url
@@ -99,66 +98,75 @@ class SearchBar(ctk.CTkFrame):
             if os.path.isdir(map_path):
                 del pending_dict[map_name]
 
-        download_result = True
-
-        if len(pending_dict) <= 40 and len(pending_dict) != 0:
-            for map_name, map_url in pending_dict.items():
-                filename = map_name + ".zip"
-                download_file = requests.get(map_url)
-
-                if download_file.status_code == 200:
-                    with open(filename, "wb") as file:
-                        file.write(download_file.content)
-
-                    extract_path = os.path.join(self.local_map_path, map_name)
-                    os.makedirs(extract_path, exist_ok=True)
-
-                    with zipfile.ZipFile(filename, "r") as zip_ref:
-                        zip_ref.extractall(extract_path)
-
-                    os.remove(filename)
-                else:
-                    download_result = False
+        # open download window
+        if self.download_window is None or not self.download_window.winfo_exists():
+            self.download_window = DLWindow(self, pending_dict)
         else:
-            download_result = False
+            self.download_window.update_content(pending_dict)
 
-        self.open_dlwindow(download_result, pending_dict)
+        self.download_window.deiconify()
 
     def toogle_tracking(self):
         self.tracker.toogle_tracking()
 
-    def open_dlwindow(self, download_result, pending_dict):
-        if self.download_window is None or not self.download_window.winfo_exists():
-            self.download_window = DLWindow(self, download_result, pending_dict)
-        else:
-            self.download_window.update_content(download_result, pending_dict)
-
-        self.download_window.deiconify()
-
 
 class DLWindow(ctk.CTkToplevel):
-    def __init__(self, parent, download_result, pending_dict, *args, **kwargs):
+    def __init__(self, parent, pending_dict, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         self.geometry("400x300")
+        self.pending_dict = pending_dict
+
+        self.map_url = "https://ra3.z31.xyz/?s="
+        self.local_map_path = r"C:\Users\mikel\AppData\Roaming\Red Alert 3\Maps"
+
+        self.combobox = ctk.CTkOptionMenu(
+            self, values=list(pending_dict.keys()), width=300
+        )
+        self.combobox.grid(row=0, column=0, padx=(0, 0))
+
+        self.rowconfigure((0, 1, 2), weight=1)
+        self.columnconfigure((0), weight=1)
+
+        self.button = ctk.CTkButton(
+            self, text="Download", command=self.start_download, width=120
+        )
+        self.button.grid(row=1, column=0, padx=(0, 0))
+
         self.label = ctk.CTkLabel(self, text="")
-        self.label.pack(padx=20, pady=20)
-        self.update_content(download_result, pending_dict)
+        self.label.grid(row=2, column=0, padx=(0, 0))
 
-    def update_content(self, download_result, pending_dict):
-        content = ""
-        if download_result:
-            content = "Download successful\n\n"
-            for map_name in pending_dict:
-                content += f"{map_name}\n"
+        self.update_content(pending_dict)
+
+    def download_map(self):
+        filename = self.combobox.get() + ".zip"
+        self.label.configure(text=f"Downloading {filename}...")
+        url = self.pending_dict.get(self.combobox.get(), None)
+
+        if url:
+            download_file = requests.get(url)
+
+            if download_file.status_code == 200:
+                with open(filename, "wb") as file:
+                    file.write(download_file.content)
+
+                extract_path = os.path.join(self.local_map_path, filename)
+                os.makedirs(extract_path, exist_ok=True)
+
+                with zipfile.ZipFile(filename, "r") as zip_ref:
+                    zip_ref.extractall(extract_path)
+
+                os.remove(filename)
+                self.label.configure(text=f"Downloaded {filename}")
+            else:
+                self.label.configure(text=f"Failed to download {filename}")
+
         else:
-            content = "Download failed\n\n"
-            if len(pending_dict) == 0:
-                content += "No maps found\n\n"
-            elif len(pending_dict) > 2:
-                content += f"Too many maps found({len(pending_dict)})\n\n"
-                for i, map_name in enumerate(pending_dict):
-                    if i >= 10:
-                        break
-                    content += f"{map_name}\n"
+            self.label.configure(text=f"Failed to find {filename} link")
 
-        self.label.configure(text=content)
+    def update_content(self, pending_dict):
+        self.pending_dict = pending_dict
+        if pending_dict:
+            self.combobox.configure(values=list(pending_dict.keys()))
+
+    def start_download(self):
+        threading.Thread(target=self.download_map).start()
